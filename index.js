2d912383dd9afb9cddc37a6be2cd111ca41a56e6
tonyalcorn <xmastony@gmail.com>
2020-11-19 10:14:19 -0800

    commit tree b3c8e9b761a1be6db8c8b660eec382eccc7f8156

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+
+function submitData(username, email) {
+    fetch('http://localhost:3000/users')
+    .then(res => res.json())
+    .then(json => console.log(json))
+}
+
+
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   })
+// });
+
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+  
+//   let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configObj);
